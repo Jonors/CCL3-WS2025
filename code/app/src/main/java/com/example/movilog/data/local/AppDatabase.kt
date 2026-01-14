ecad715dfa9f128981f8094eced4482f3240782b
paulhitzl <cc241055@fhstp.ac.at>
2026-01-14 10:01:09 +0100

    Stats view implemented 2

--- code/app/src/main/java/com/example/movilog/data/local/AppDatabase.kt
+++ code/app/src/main/java/com/example/movilog/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.movilog.data.model.Movie
 
-@Database(entities = [Movie::class], version = 1, exportSchema = false)
+@Database(entities = [Movie::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
@@ -21,7 +21,8 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "movie_database"
-                ).build()
+                ).fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
